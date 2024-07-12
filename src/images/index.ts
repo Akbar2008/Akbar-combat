8b249608e423701c0f04bfeaa08e7bcb9a9c9758
Akbar <akbar.husanov0606@gmail.com>
2024-07-12 08:29:50 +0500

    Combo

---
+++ src/images/index.ts
@@ -0,0 +1,27 @@
+import hamsterExchange from "./hamster-exchange.png";
+import binanceLogo from "./binance-logo.png";
+import dollarCoin from "./dollar-coin.png";
+import dailyReward from "./daily-reward.png";
+import dailyCipher from "./daily-cipher.png";
+import dailyCombo from "./daily-combo.png";
+import hamsterCoin from "./hamster-coin.png";
+import mainCharacter from "./main-character.png";
+import master from "./master.png";
+import bronze from "./Bronze.png";
+import silver from "./Silver.png";
+import gold from "./gold.png";
+
+export {
+  hamsterExchange,
+  binanceLogo,
+  dollarCoin,
+  dailyReward,
+  dailyCipher,
+  dailyCombo,
+  hamsterCoin,
+  mainCharacter,
+  bronze,
+  master,
+  silver,
+  gold,
+};
